57a33224afb567271b26f760701a952386986fe6
MaciejChyrc <222112@student.pwr.edu.pl>
2016-06-06 09:51:48 +0200

    Update Armor.cpp

--- Armor.cpp
+++ Armor.cpp
@@ -11,7 +11,7 @@ Armor::~Armor()
 {
 }
 
-double Armor::ReduceDmgPercent ()
+double Armor::ReduceDmgPercent ()	//max protection to 600, przy wiekszej wartosci zbroja zaczyna leczyc wlasciciela, nawet ponad max hp
 {
 	return (double)Protection / 6 / 100;	//np przy 60 protection pancerz bedzie zbijal 60/6 -> 10% obrazen
 }
